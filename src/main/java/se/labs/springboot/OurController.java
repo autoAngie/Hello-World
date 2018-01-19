9ae2da20ef40980d1918e125ae6338a40c38a8e8
Angela Gerlitz <Angelas@pro.local>
2018-01-19 11:31:07 +0100

    kommentar

--- src/main/java/se/labs/springboot/OurController.java
+++ src/main/java/se/labs/springboot/OurController.java
@@ -25,7 +25,7 @@ public class OurController {
     @RequestMapping("/yo/{name}")
     @ResponseBody
     String yo(@PathVariable String name) {
-        return "YO " + name;
+        return "Tjaba " + name;
     }
 
     public static void main(String[] args) throws Exception {
